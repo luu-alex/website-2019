3ceb946f47be35cbfb4663f03ed4660a06b3c6be
Alex Luu <Alex.luu@loopio.com>
2019-11-15 23:05:50 -0500

    adding ui

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
+
+Vue.component('screen', {
+    props: ['item'],
+    template: '<p>{{item.text}}</p>'
+})
+
+Vue.component('v-input', {
+    template: '<input/>',
+    props: ['message']
+})
+
+Vue.component('v-submit', {
+    template:'<button/>',
+    data: function() {
+        return ['message']
+    }
+})
+
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
-  }
+    list: [
+    ],
+    message: '',
+    nextId: 0
+},
+methods: {
+    submitCommand: function() {
+        this.list.push({
+            id: this.nextId++,
+            text: 'User$ '+this.message
+        })
+        this.message = ''
+    }
+}
 })
