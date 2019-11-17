21a3f4820bd5900f5ff4dc9ba2429c4834832e1b
alex <luu.alex@outlook.com>
2019-11-17 00:53:25 -0500

    changed how input works and added a few commands!

--- index.js
+++ index.js
@@ -4,29 +4,34 @@ Vue.component('screen', {
     template: '<p>{{item.text}}</p>'
 })
 
-Vue.component('v-input', {
-    template: '<input/>',
-    props: ['message']
+Vue.component('v-mac-tabs', {
+    template: `
+    <div>
+        <i class="fas fa-circle" style="color:red"></i>
+        <i class="fas fa-circle" style="color:green"></i>
+        <i class="fas fa-circle" style="color:grey"></i>
+    </div>
+    `
 })
-
-Vue.component('v-submit', {
-    template:'<button/>',
-    data: function() {
-        return ['message']
-    }
-})
-
-Vue.component('v-output', {
-    props: ['inp'],
-    template:`<p>{{inp.text}}</p>`
-})
-
 var app = new Vue({
   el: '#app',
   data: {
-    list: [],
+    list: [
+        {
+            id:1, 
+            text:'Welcome! My name is Alex Luu and I am a Third year UofT student studying Computer Science, I\'m currently interning at Toronto start up Loopio. '
+        }, 
+        {
+            id:2, 
+            text:'I\'m a passionate programmer attending Hackathons such as Uoft Hacks and Hack the North and on my free time and love bringing ideas to life through side projects' 
+        },
+        {
+            id:3,
+            text:"For more commands enter 'help'"
+        }
+    ],
     message: '',
-    nextId: 0,
+    nextId: 3,
     inp: ''
 
 },
@@ -34,20 +39,32 @@ methods: {
     submitCommand: function() {
         this.list.push({
             id: this.nextId++,
-            text: 'User$ '+this.message
+            text: 'User$ '+this.inp
         })
-        this.message = ''
+        this.inp = ''
     },
     changeInp: function(char) {
-        this.inp += char;
-        console.log(this.inp);
+        if(this.inp.length <= 50) {
+            this.inp += char;
+        }
+    },
+    removeLastInp: function() {
+        if(this.inp.length > 0) {
+            this.inp = this.inp.substring(0, this.inp.length - 1);
+        }
     }
 },
 mounted() {
     window.addEventListener("keypress", function(e) {
-      console.log(String.fromCharCode(e.keyCode));
       this.changeInp(String.fromCharCode(e.keyCode));
-
     }.bind(this));
+
+    window.addEventListener("keydown", function(e) {
+        if (e.keyCode == 8) {
+            this.removeLastInp();
+        } else if (e.keyCode == 13) {
+            this.submitCommand();
+        }
+      }.bind(this));
   }
 })
