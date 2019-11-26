63b62d6152cbfeeb8e5c041cf288425543fa047a
Alex Luu <Alex.luu@loopio.com>
2019-11-25 23:57:34 -0500

    queue functionality

--- index.js
+++ index.js
@@ -1,7 +1,63 @@
+function TimerQueue(){
+  this.currentTimer = null;
+  this.tasks = [];
+}
+
+TimerQueue.prototype.addTask = function(callback, delay){
+  this.tasks.push({ callback: callback, delay: delay });
+
+  // If there's a scheduled task, bail out.
+  if(this.currentTimer) return;
+
+  // Otherwise, start kicking tires
+  this.launchNextTask();
+};
+
+TimerQueue.prototype.launchNextTask = function(){
+
+  // If there's a scheduled task, bail out.
+  if(this.currentTimer) return;
+
+  var self = this;
+  var nextTask = this.tasks.shift();
+
+  // There's no more tasks, clean up.
+  if(!nextTask) return this.clear();
+
+  // Otherwise, schedule the next task.
+  this.currentTimer = setTimeout(function(){
+    nextTask.callback.call();
+
+    self.currentTimer = null;
+
+    // Call this function again to set up the next task.
+    self.launchNextTask();
+  }, nextTask.delay);
+};
+
+TimerQueue.prototype.clear = function(){
+  if (this.currentTimer) clearTimeout(this.currentTimer);
+
+  // Timer clears only destroy the timer. It doesn't null references.
+  this.currentTimer = null;
+
+  // Fast way to clear the task queue
+  this.tasks.length = 0;
+};
+
+var queue = new TimerQueue();
+var id = 1;
 var phrases = {
     help : "this is the help tab!",
     about : "this is the about tab!"
 }
+
+function delay(t, v) {
+   return new Promise(function(resolve) {
+       setTimeout(resolve.bind(null, v), t)
+   });
+}
+
 Vue.component('screen', {
     props: ['item'],
     template: '<p>{{item.text}}</p>'
@@ -19,33 +75,21 @@ Vue.component('v-mac-tabs', {
 var app = new Vue({
   el: '#app',
   data: {
-    list: [
-        {
-            id:1,
-            text:'Welcome! My name is Alex Luu and I am a Third year UofT student studying Computer Science, I\'m currently interning at Toronto start up Loopio. '
-        },
-        {
-            id:2,
-            text:"For more commands enter 'help'"
-        }
-    ],
+    list: [],
     nextId: 3,
     inp: ''
 
 },
 methods: {
     submitCommand: function() {
-        if (this.inp == "help") {
-            this.list.push({
-                id: this.nextId++,
-                text: 'help'
-            })
+        if (phrases.hasOwnProperty(this.inp)) {
+            this.commandList(this.inp);
         }
         this.list.push({
-            id: this.nextId++,
+            id: id++,
             text: 'User$ '+this.inp
-        })
-        //this.inp = ''
+        });
+        this.inp = '';
     },
     changeInp: function(char) {
         if(this.inp.length <= 50) {
@@ -57,29 +101,42 @@ methods: {
             this.inp = this.inp.substring(0, this.inp.length - 1);
         }
     },
-    commandList: function() {
-        if (this.inp == "help") {
-            this.list.push(
-                {
-                    id: this.nextId++,
-                    text: "help cd ls other things that are for commnads"
-                }
-            )
-        } else if (this.inp == "about") {
-            this.list.push(
-                {
-                    id: this.nextId++,
-                    text: "this is the about text"
-                }
-            )
+    commandList: function(phrase) {
+        phrase = phrase.toLowerCase();
+        if (phrase == "help") {
+            this.pushPhrase("To learn more about me enter 'about'");
+            this.pushPhrase("If you are curous on how I built this, enter 'how'");
+        } else if (phrase == "about") {
+            this.pushPhrase("I currently intern at Loopio, a start up in Toronto as a full-stack Software developer");
+            this.pushPhrase("I worked for the University of Toronto as a Teaching Assistant for Computer Science");
+            this.pushPhrase("I've attended Hack the North and UofT Hacks, they were both great opportunities meeting amazing people");
+            this.pushPhrase("And was able to bring some amazing ideas to life :)");
+        } else if (phrase == "how") {
+            this.pushPhrase("This site was built using Vue.js and Javascript by me :), the source code is available on my Github.");
+            this.pushPhrase("I use a queue to push strings that will show up on to this screen!");
+            this.pushPhrase("And I use key press events to display text when you type");
         }
-        console.log(this.list)
     },
+    pushPhrase: function(phrase, delay = 1000) {
+        var lst = this.list;
+
+        queue.addTask(function () {
+            lst.push(
+                {
+                    id: id++,
+                    text: phrase
+                }
+            )
+        }, delay);
+
+    }
 },
 mounted() {
     window.addEventListener("keypress", function(e) {
-      this.changeInp(String.fromCharCode(e.keyCode));
-      console.log(String.fromCharCode(e.keyCode))
+        if (e.keyCode !=13){
+          this.changeInp(String.fromCharCode(e.keyCode));
+          console.log(String.fromCharCode(e))
+        }
     }.bind(this));
 
     window.addEventListener("keydown", function(e) {
@@ -89,5 +146,8 @@ mounted() {
             this.submitCommand();
         }
       }.bind(this));
+      this.pushPhrase('Welcome!');
+      this.pushPhrase('My name is Alex Luu and I am a Third year UofT student studying Computer Science');
+      this.pushPhrase("For more commands enter 'help'", 1000);
   }
 })
